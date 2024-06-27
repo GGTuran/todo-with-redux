8f5ec567020f9ce2fd306d516d41c2644d24029b
GGTuran <tayyabturan67@gmail.com>
2024-06-27 21:17:08 +0600

    init

---
+++ src/components/todo/TodoContainer.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { useAppSelector } from "@/redux/hook";
+import AddTodoModal from "./AddTodoModal";
+import TodoCard from "./TodoCard";
+import TodoFilter from "./TodoFilter";
+import { useGetTodosQuery } from "@/redux/api/api";
+
+const TodoContainer = () => {
+  //from local state
+  // const { todos } = useAppSelector((state) => state.todos);
+
+  //from server
+  const { data: todos, isLoading, isError } = useGetTodosQuery(undefined);
+  console.log(todos);
+
+  if(isLoading){
+    return <p>Loading...</p>
+  }
+
+  return (
+    <div>
+      <div className="flex justify-between mb-5">
+        <AddTodoModal></AddTodoModal>
+        <TodoFilter></TodoFilter>
+      </div>
+      <div className="bg-slate-500 w-full h-full rounded-xl p-[5px]">
+        <div className="bg-white p-5 w-full h-full space-y-3  rounded-lg">
+          {todos?.data?.map((item) => (
+            <TodoCard
+              {...item}
+            ></TodoCard>
+          ))}
+        </div>
+        {/* <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center">
+            <p>There is no task pending</p>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default TodoContainer;
